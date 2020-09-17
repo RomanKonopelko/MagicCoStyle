61fdcff309eb176a005ec9740609cd922c2168dc
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-09-17 19:56:53 +0300

    js

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,4 +3,31 @@ $('.slider').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+  ],
 });
