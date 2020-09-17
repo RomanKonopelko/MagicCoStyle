4f639f476f2a441ae0ef82ceb6b40fa67330c671
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-09-17 22:05:28 +0300

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,33 +1,45 @@
-$('.slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  responsive: [
-    {
-      breakpoint: 1440,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
+jQuery(document).ready(function ($) {
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    focusOnSelect: true,
+    slidesToShow: 1,
+    asNavFor: '.thumbnail-slider',
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
       },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
       },
-    },
-    {
-      breakpoint: 320,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
       },
-    },
-  ],
+    ],
+  });
+  $('.thumbnail-slider').slick({
+    slidesToShow: 7,
+    infinite: true,
+    asNavFor: '.slider',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
 });
