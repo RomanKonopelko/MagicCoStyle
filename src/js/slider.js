0b001493af593c0b457722a0eeaa2800e88bd4de
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-09-19 01:03:40 +0300

    test

--- src/js/slider.js
+++ src/js/slider.js
@@ -14,6 +14,7 @@ jQuery(document).ready(function ($) {
         breakpoint: 768,
         settings: {
           asNavFor: false,
+          adaptiveWidth: true,
           adaptiveHeight: true,
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -29,6 +30,7 @@ jQuery(document).ready(function ($) {
     arrows: false,
     draggable: false,
     infinite: true,
+    itemWidth: 500,
     asNavFor: '.slider',
     dots: false,
     centerMode: true,
